0d1e1d5940bcfd6350866d8e15d4a96aa0e65112
Vladimir Shushlin(v-sh) <v.shushlin@gmail.com>
2015-07-29 09:38:04 +0300

    remove workarounds

--- lib/voximplant_api/client.rb
+++ lib/voximplant_api/client.rb
@@ -19,49 +19,8 @@ module VoximplantApi
       @api_key = options[:api_key]
     end
 
-    def get_info
-      perform_request("GetAccountInfo")["result"]
-    end
-
-    def get_phone_number_categories(options)
-      perform_request("GetPhoneNumberCategories", options)["result"][0]["phone_categories"]
-    end
-
-    #options: country_code, phone_category_name
-    def get_phone_number_regions(options)
-      perform_request("GetPhoneNumberRegions", options)["result"]
-    end
-
-    def get_new_phone_numbers(options)
-      perform_request("GetNewPhoneNumbers", options)["result"]
-    end
-
-    def get_phone_numbers(options)
-      perform_request("GetPhoneNumbers", options)["result"]
-    end
-
-    def attach_phone_number(options)
-      perform_request("AttachPhoneNumber", options)["phone_numbers"]
-    end
-
-    def get_scenarios(options)
-      perform_request("GetScenarios", options)["result"]
-    end
-
-    def get_rules(options)
-      perform_request("GetRules", options)["result"]
-    end
-
-    def bind_phone_number_to_application(options)
-      perform_request("BindPhoneNumberToApplication", options)
-    end
-
     def create_child_account(options)
       perform_request_as_parent("AddAccount", options)
-    end
-
-    def get_money_amount_to_charge(options)
-      perform_request("GetMoneyAmountToCharge", options)["result"]
     end
 
     def method_missing(name, *args)
